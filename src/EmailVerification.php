c01fc00448e85b7248571d8d8249fa3889904b3d
Ahmed Ali <dev.ahmed.abbas@gmail.com>
2017-10-13 21:21:14 +0200

    adding test case for invalid email parameters

---
+++ src/EmailVerification.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmed ali
+ * Date: 10/13/2017
+ * Time: 8:55 PM
+ */
+
+namespace PHPVerifyEmail;
+
+
+class EmailVerification
+{
+
+}
---
+++ tests/AbstractVerifyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPVerifyEmail\PHPVerifyEmail as EmailVerifier;
+
+class AbstractVerifyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testThrowExceptionWhenInvalidEmail()
+    {
+        $email = 'dev.ahmed.abbas-gmail.com';
+        $verify_email = 'dev.ahmed.abbas@gmail.com';
+        $port = 26;
+
+        EmailVerifier::verify( $email, $verify_email, $port );
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testThrowExceptionWhenInvalidVerifierEmail()
+    {
+        $email = 'dev.ahmed.abbas@gmail.com';
+        $verify_email = 'dev.ahmed.abbas-gmail.com';
+        $port = 26;
+
+        EmailVerifier::verify( $email, $verify_email, $port );
+    }
+
+}
